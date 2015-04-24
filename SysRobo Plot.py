621b233cc46b719e576a54612d1c50fa6ef0b25a
Michelle Sit <msit@wellesley.edu>
2015-04-24 19:37:15 -0400

    removed random third window thing

--- SysRobo Plot.py
+++ SysRobo Plot.py
@@ -4,10 +4,7 @@ import numpy as np
 from pylab import *
 import matplotlib.pyplot as plt
 
-ax=plt.gca()
-
 fig = plt.figure()
-ax.clear()
 
 XSqrPts = [2, 16, 16, 2, 2, 16, 16, 2, 2, 19, 19, 1, 0]
 YSqrPts = [3, 3, 7, 7, 11, 11, 15, 15, 19, 19, 1, 1, 0]
@@ -15,7 +12,6 @@ YSqrPts = [3, 3, 7, 7, 11, 11, 15, 15, 19, 19, 1, 1, 0]
 line = plt.plot(XSqrPts, YSqrPts, 'r-')
 
 fig2 = plt.figure()
-ax.clear()
 axis('equal')
 plt.xlim(0,2)
 plt.ylim(0,2)
